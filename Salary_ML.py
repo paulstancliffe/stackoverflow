a56023193a840bccc57ba5f9558c7312e2f0fe99
Paul Stancliffe <paul.stancliffe@gmail.com>
2019-01-21 18:07:38 +0100

    import torch added

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pytorch
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
